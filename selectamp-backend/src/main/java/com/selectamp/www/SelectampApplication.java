5e08f9589a824405b62bfe485b1e2403fed65ecd
selectjun <selectjun@gmail.com>
2021-02-11 15:34:37 +0900

    chore: init backend

---
+++ selectamp-backend/src/main/java/com/selectamp/www/SelectampApplication.java
@@ -0,0 +1,13 @@
+package com.selectamp.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectampApplication.class, args);
+    }
+
+}
