9e3ceb154648cfd7ef03eafa58b894870471e103
selectjun <selectjun@gmail.com>
2021-03-04 21:38:35 +0900

    fix: pagination component.

--- selectamp-frontend/src/components/Pagination/Pagination.tsx
+++ selectamp-frontend/src/components/Pagination/Pagination.tsx
@@ -13,25 +13,26 @@ export type PaginationProps = {
 export default function Pagination({ url, totalCount, currentPage, countPerPage, numberOfPagination = 10 }: PaginationProps) {
   const firstPageNumber: number = 1;
   const prevPageNumber: number = Math.floor(currentPage / numberOfPagination) - 1 < 0 ? 1 : (Math.floor(currentPage / numberOfPagination) - 1)  * 10 + 1;
-  const startPageNumber: number = Math.floor(currentPage / numberOfPagination) * 10 + 1;
-  const endPageNumber: number = Math.floor(currentPage / numberOfPagination) * 10 + 10 > Math.floor(totalCount / countPerPage) ? Math.floor(totalCount / countPerPage) : Math.floor(currentPage / numberOfPagination) * 10 + 10;
-  const nextPageNumber: number = (Math.floor(currentPage / numberOfPagination) + 1) * 10 + 1 > Math.floor(totalCount / countPerPage) ? Math.floor(totalCount / countPerPage) : (Math.floor(currentPage / numberOfPagination) + 1)  * 10 + 1;
+  const startPageNumber: number = currentPage % numberOfPagination === 0 ? (Math.floor(currentPage / numberOfPagination) - 1) * 10 + 1 : Math.floor(currentPage / numberOfPagination) * 10 + 1;
+  const endPageNumber: number = startPageNumber + numberOfPagination - 1 > Math.floor(totalCount / countPerPage) ? Math.floor(totalCount / countPerPage) : startPageNumber + numberOfPagination - 1;
+  const nextPageNumber: number = startPageNumber + numberOfPagination > Math.floor(totalCount / countPerPage) ? Math.floor(totalCount / countPerPage) : startPageNumber + numberOfPagination
   const lastPageNumber: number = Math.floor(totalCount / countPerPage);
   const pageNumberItems: Array<number> = [];
 
+  console.log(endPageNumber);
   for (let page = startPageNumber; page <= endPageNumber; page++) { pageNumberItems.push(page); };
 
   return (
     pageNumberItems.length
     ? <div css={PaginationStyle}>
       <ul>
-        <li><Link to={`/community?page=${firstPageNumber}`}>&lt;&lt;</Link></li>
-        <li><Link to={`/community?page=${prevPageNumber}`}>&lt;</Link></li>
+        <li><Link to={`${url}?page=${firstPageNumber}`}>&lt;&lt;</Link></li>
+        <li><Link to={`${url}?page=${prevPageNumber}`}>&lt;</Link></li>
         {
           pageNumberItems.map((item, index) => <PaginationItem key={index} url={url} paginationNumber={item} isActive={item === currentPage} />)
         }
-        <li><Link to={`/community?page=${nextPageNumber}`}>&gt;</Link></li>
-        <li><Link to={`/community?page=${lastPageNumber}`}>&gt;&gt;</Link></li>
+        <li><Link to={`${url}?page=${nextPageNumber}`}>&gt;</Link></li>
+        <li><Link to={`${url}?page=${lastPageNumber}`}>&gt;&gt;</Link></li>
       </ul>
     </div>
     : null
