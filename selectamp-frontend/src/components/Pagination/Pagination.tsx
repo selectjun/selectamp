0284e23945c46f8089958d234052f758f32fd418
selectjun <selectjun@gmail.com>
2021-04-02 20:40:03 +0900

    feat: add pagination

--- selectamp-frontend/src/components/Pagination/Pagination.tsx
+++ selectamp-frontend/src/components/Pagination/Pagination.tsx
@@ -1,16 +1,17 @@
 import { css } from '@emotion/react';
 import { Link } from 'react-router-dom';
 import PaginationItem from './PaginationItem';
+import Icon from '../Icon';
 
 export type PaginationProps = {
   url: string,
-  totalCount: number,
-  currentPage: number,
-  countPerPage: number,
+  totalCount?: number,
+  currentPage?: number,
+  countPerPage?: number,
   numberOfPagination?: number
 };
 
-export default function Pagination({ url, totalCount, currentPage, countPerPage, numberOfPagination = 10 }: PaginationProps) {
+export default function Pagination({ url, totalCount = 0, currentPage = 1, countPerPage = 10, numberOfPagination = 10 }: PaginationProps) {
   const firstPageNumber: number = 1;
   const prevPageNumber: number = Math.floor(currentPage / numberOfPagination) - 1 < 0 ? 1 : (Math.floor(currentPage / numberOfPagination) - 1)  * 10 + 1;
   const startPageNumber: number = currentPage % numberOfPagination === 0 ? (Math.floor(currentPage / numberOfPagination) - 1) * 10 + 1 : Math.floor(currentPage / numberOfPagination) * 10 + 1;
@@ -19,27 +20,37 @@ export default function Pagination({ url, totalCount, currentPage, countPerPage,
   const lastPageNumber: number = Math.floor(totalCount / countPerPage);
   const pageNumberItems: Array<number> = [];
 
-  console.log(endPageNumber);
   for (let page = startPageNumber; page <= endPageNumber; page++) { pageNumberItems.push(page); };
+
+  const handleMoveScrollPageTop = (): void => { window.scrollTo(0, 0); }
 
   return (
     pageNumberItems.length
     ? <div css={PaginationStyle}>
       <ul>
-        <li><Link to={`${url}?page=${firstPageNumber}`}>&lt;&lt;</Link></li>
-        <li><Link to={`${url}?page=${prevPageNumber}`}>&lt;</Link></li>
+        <li onClick={handleMoveScrollPageTop}><Link to={`${url}?page=${firstPageNumber}`}><Icon name="first" style={paginationItemIconStyle} /></Link></li>
+        <li onClick={handleMoveScrollPageTop}><Link to={`${url}?page=${prevPageNumber}`}><Icon name="prev" style={paginationItemIconStyle} /></Link></li>
         {
-          pageNumberItems.map((item, index) => <PaginationItem key={index} url={url} paginationNumber={item} isActive={item === currentPage} />)
+          pageNumberItems.map((item, index) => <PaginationItem key={index} url={url} paginationNumber={item} isActive={item === currentPage} onHandleMoveScrollPageTop={handleMoveScrollPageTop} />)
         }
-        <li><Link to={`${url}?page=${nextPageNumber}`}>&gt;</Link></li>
-        <li><Link to={`${url}?page=${lastPageNumber}`}>&gt;&gt;</Link></li>
+        <li onClick={handleMoveScrollPageTop}><Link to={`${url}?page=${nextPageNumber}`}><Icon name="next" style={paginationItemIconStyle} /></Link></li>
+        <li onClick={handleMoveScrollPageTop}><Link to={`${url}?page=${lastPageNumber}`}><Icon name="last" style={paginationItemIconStyle} /></Link></li>
       </ul>
     </div>
     : null
   );
 };
 
+const paginationItemIconStyle = {
+  "width": "1.5rem",
+  "height": "1.5rem"
+}
+
 const PaginationStyle = css`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+
   ul {
     list-style: none;
     padding: 0;
@@ -49,9 +60,43 @@ const PaginationStyle = css`
     li {
       display: inline-block;
       padding: 0.5rem;
+      width: 1.5rem;
+      height: 1.5rem;
+      float: left;
+      border-top: 1px solid #aaa;
+      border-bottom: 1px solid #aaa;
+      border-left: 1px solid #aaa;
 
-      &.active a {
-        color: red;
+      
+      &:first-of-type {
+        border-top-left-radius: 0.5em;
+        border-bottom-left-radius: 0.5rem;  
+      }
+
+      &:last-of-type {
+        border-right: 1px solid #aaa;
+        border-top-right-radius: 0.5rem;
+        border-bottom-right-radius: 0.5rem;  
+      }
+
+      a {
+        text-decoration: none;
+        color: #000;
+      }
+
+      &.active {
+        background: #005CB2;
+        a {
+          color: #fff;
+          font-weight: bold;
+        }
+      }
+
+      &:hover {
+        cursor: pointer;
+        a {
+          font-weight: bold;
+        }
       }
     }
   }
--- selectamp-frontend/src/components/Pagination/PaginationItem.tsx
+++ selectamp-frontend/src/components/Pagination/PaginationItem.tsx
@@ -1,15 +1,22 @@
+import { css } from '@emotion/react';
 import { Link } from 'react-router-dom';
 
 export type PaginationItemProps = {
   url: string,
   paginationNumber: number,
-  isActive: boolean
+  isActive: boolean,
+  onHandleMoveScrollPageTop: () => void
 };
 
-export default function PaginationItem({ url, paginationNumber, isActive }: PaginationItemProps) {
+export default function PaginationItem({ url, paginationNumber, isActive, onHandleMoveScrollPageTop }: PaginationItemProps) {
   return (
-    <li className={isActive ? "active" : ""}>
+    <li className={isActive ? "active" : ""} css={paginationItemStyle} onClick={onHandleMoveScrollPageTop}>
       <Link to={`${url}?page=${paginationNumber}`}>{paginationNumber}</Link>
     </li>
   );
 };
+
+const paginationItemStyle = css`
+  a {
+  }
+`;
