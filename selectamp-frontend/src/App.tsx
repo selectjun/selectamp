f4deb0d15abd0328943094c63dcd3c1847a65d28
selectjun <selectjun@gmail.com>
2021-05-02 15:31:01 +0900

    chore: add etc.

--- selectamp-frontend/src/App.tsx
+++ selectamp-frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Dashboard from './pages/Dashboard';
 import Pamphlet from './pages/Pamphlet';
 import CommunityView from './pages/Community/CommunityView';
 import CommunityWrite from './pages/Community/CommunityWrite';
+import CommunityModify from './pages/Community/CommunityModify';
 import CommunityContainer from './containers/Community/CommunityContainer';
 import Guide from './pages/Guide';
 import Notice from './pages/Notice';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/pamphlet" component={Pamphlet} />
         <Route path="/community/write" component={CommunityWrite} />
+        <Route path="/community/:id/modify" component={CommunityModify} />
         <Route path="/community/:id" component={CommunityView} />
         <Route path="/community" component={CommunityContainer} />
         <Route path="/guide" component={Guide} />
